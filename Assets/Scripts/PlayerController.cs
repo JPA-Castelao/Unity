6a35977f88addb8b5ad620828a7c6c94335704a1
Jerson <jpinedaarenas@danielcastelao.org>
2026-02-10 14:45:20 +0100

    Add files via upload

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0; 
+    public TextMeshProUGUI countText;
     private Rigidbody rb; 
+    private int count;
     private float movementX;
     private float movementY;
     // Start is called before the first frame update
     void Start()
     {
+        count = 0;
         rb = GetComponent <Rigidbody>(); 
     }   
     void OnMove (InputValue movementValue)
@@ -24,6 +28,15 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
         rb.AddForce(movement*speed);
         
+    }
+    
+    void OnTriggerEnter (Collider other) 
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+            count=count+1
+        }
     }
     // Update is called once per frame
     
